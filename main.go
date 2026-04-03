5d2b454bac8f74fb9b3855ede36c83daabb5b257
Maksim Kostromin <daggerok@gmail.com>
2026-04-03 19:38:28 -0400

    add address to home page

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	http.HandleFunc("/", handlers.HomeHandler)
 	fmt.Println("Server running on port 8081")
+	fmt.Println("Visit http://localhost:8081 to see the home page")
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		fmt.Println("Server failed to start:", err)
 	}
